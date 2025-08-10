464b6f0f64754ee842d1b19212bf1e7344048f57
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-08-10 21:43:04 +0530

    Time: 1 ms (85.79%) | Memory: 40.9 MB (55.26%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,31 +1,16 @@
 class Solution {
-    public static boolean permutation(char arr[], int i, int j) {
-        if (i == j) {
-            String s = new String(arr);
-            if (s.charAt(0) != '0') {
-                int number = Integer.parseInt(s);
-                return (number > 0 && (number & (number - 1)) == 0);
-            }
-            return false;
-        }
-
-        for (int k = i; k <= j; k++) {
-            swap(arr, i, k);
-            if (permutation(arr, i + 1, j)) return true; 
-            swap(arr, i, k);
+    public boolean reorderedPowerOf2(int n) {
+        String target = sort(n);
+        for (int i = 0; i < 30; i++) {
+            int power = 1 << i;
+            if (target.equals(sort(power))) return true;
         }
         return false;
     }
 
-    public static void swap(char arr[], int i, int j) {
-        char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    public boolean reorderedPowerOf2(int n) {
-        String s = String.valueOf(n);
-        char arr[] = s.toCharArray();
-        return permutation(arr, 0, arr.length - 1);
+    public String sort(int n) {
+        char[] arr = String.valueOf(n).toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
     }
 }
