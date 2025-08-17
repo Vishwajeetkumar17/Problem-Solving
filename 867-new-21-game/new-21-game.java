8953e34e17bc1e8a123845040e599089060e79a1
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-08-17 15:33:43 +0530

    Time: 3 ms (100.00%) | Memory: 43.2 MB (96.63%) - LeetSync

---
+++ 867-new-21-game/new-21-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k==0 || n>=k-1 + maxPts) return 1.0;
+        double[] dp = new double[maxPts];
+        dp[0] = 1.0;
+        double sum = 1.0, res = 0.0;
+        for(int i=1; i<=n; i++){
+            double prob = sum/maxPts;
+            if (i < k) sum+= prob;
+            else res += prob;
+            if (i >= maxPts) sum -= dp[i%maxPts];
+            dp[i % maxPts] = prob;
+        }
+        return res;
+    }
+}
