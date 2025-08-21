92c984b00925efea396bd867483d9cb3435ae94d
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-08-21 23:53:06 +0530

    Time: 49 ms (53.00%) | Memory: 47.5 MB (73.33%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,26 +1,24 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return atMostK(nums, k) - atMostK(nums, k - 1);
+        return atMost(nums, k) - atMost(nums, k-1);
     }
-
-    private int atMostK(int[] nums, int k) {
-        int i = 0, res = 0;
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int j = 0; j < nums.length; j++) {
-            freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
-            if (freq.get(nums[j]) == 1) {
-                k--;
-            }
-
-            while (k < 0) {
-                freq.put(nums[i], freq.get(nums[i]) - 1);
-                if (freq.get(nums[i]) == 0) {
-                    k++;
+    public int atMost(int[] nums, int k)
+    {
+        Map<Integer,Integer> map = new HashMap<>();
+        int i = 0, j = 0, cnt = 0;
+        while(j < nums.length){
+            map.put(nums[j], map.getOrDefault(nums[j],0)+1);
+            while(map.size()>k){
+                map.put(nums[i], map.get(nums[i])-1);
+                if(map.get(nums[i])==0)
+                {
+                    map.remove(nums[i]);
                 }
                 i++;
             }
-            res += j - i + 1;
+            cnt += j - i+1;
+            j++;
         }
-        return res;
+        return cnt;
     }
 }
