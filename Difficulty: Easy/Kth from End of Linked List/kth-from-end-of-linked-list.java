6bc29a69240334d4b0dee900db661371b2c2bf9a
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-08-30 13:32:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.java
@@ -0,0 +1,26 @@
+/* Structure of node
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+} */
+
+class Solution {
+
+    // Function to find the data of kth node from
+    // the end of a linked list.
+    int getKthFromLast(Node head, int k) {
+        // Your code here
+        Node fast = head, slow = head;
+        for(int i=0;i<k;i++){
+            if(fast == null) return -1;
+            fast = fast.next;
+        }
+        while(fast != null){
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow.data;
+    }
+}
