f928d6624787e74fcd03d0b388492c1600a4e16a
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-09-05 19:53:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Linked List Group Reverse/linked-list-group-reverse.java
@@ -0,0 +1,39 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node reverseKGroup(Node head, int k) {
+        // code here
+        if(head == null || k == 1){
+            return head;
+        }
+        
+        Node curr = head;
+        Node next = null;
+        Node prev = null;
+        int count = 0;
+        
+        while(curr != null && count < k){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        
+        if(next != null){
+            head.next = reverseKGroup(next,k);
+        }
+        return prev; 
+    }
+}
