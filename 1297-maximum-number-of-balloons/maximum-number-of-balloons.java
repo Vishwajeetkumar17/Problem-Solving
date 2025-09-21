4d88a1df81ed175f3144db623d56e74330ed4e8d
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-09-22 02:26:49 +0530

    Time: 2 ms (90.90%) | Memory: 42.1 MB (70.15%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.java
@@ -1,37 +1,22 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character, Integer> mpp = new HashMap<>();
-        int min = Integer.MAX_VALUE;
-        mpp.put('b', 0);
-        mpp.put('a', 0);
-        mpp.put('l', 0);
-        mpp.put('o', 0);
-        mpp.put('n', 0);
-        for(int i=0;i<text.length();i++){
-            char ch = text.charAt(i);
-            if(ch == 'b' || ch == 'a' || ch == 'l' || ch == 'o' || ch == 'n'){
-                mpp.put(ch, mpp.getOrDefault(ch, 0)+1);
-            }
+        int len = text.length();
+        int b = 0;
+        int a = 0;
+        int l = 0;
+        int o = 0;
+        int n = 0;
+       
+        for(char c : text.toCharArray()){
+            if(c == 'b') b++;
+            else if(c == 'a') a++;
+            else if(c == 'o') o++;
+            else if(c == 'l') l++;
+            else if(c == 'n') n++;
+           
         }
-        for(Map.Entry<Character, Integer> mp : mpp.entrySet()){
-            int temp = 0;
-            if(mp.getKey() == 'b'){
-                temp = mp.getValue();
-            }
-            else if(mp.getKey() == 'a'){
-                temp = mp.getValue();
-            }
-            else if(mp.getKey() == 'l'){
-                temp = mp.getValue()/2;
-            }
-            else if(mp.getKey() == 'o'){
-                temp = mp.getValue()/2;
-            }
-            else if(mp.getKey() == 'n'){
-                temp = mp.getValue();
-            }
-            min = Math.min(min, temp);
-        }
-        return min;
+        l /= 2;
+        o /= 2;
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
     }
 }
