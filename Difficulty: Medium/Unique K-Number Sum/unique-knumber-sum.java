d8aec9f33ba950968266feea1d1f378a364393d0
Vishwajeet kumar <117035670+Vishwajeetkumar17@users.noreply.github.com>
2025-10-03 00:22:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique K-Number Sum/unique-knumber-sum.java
@@ -0,0 +1,27 @@
+class Solution {
+    public ArrayList<ArrayList<Integer>> combinationSum(int n, int k) {
+        // code here
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        ArrayList<Integer> sb = new ArrayList<>();
+        int sum = 0;
+        solve(n, k, ans, sb, 0, 1);
+        return ans;
+    }
+    
+    public void solve(int n, int k,ArrayList<ArrayList<Integer>> ans, ArrayList<Integer> sb ,int sum,int st ){
+        if(sb.size() == k){
+            if(n == sum){
+                ans.add(new ArrayList<>(sb));
+                return;
+            }
+            return;
+        }
+        for(int i=st; i<=9; i++){
+            sum += i;
+            sb.add(i);
+            solve(n, k, ans, sb, sum, i+1);
+            sum -= i;
+            sb.remove(sb.size()-1);
+        }
+    }
+}
